c02a7ee9a6c681c4093a6b1ca966d5ad7a18ec42
Maneesh D <maneesh.d123@gmail.com>
2024-01-10 18:03:45 -0800

    Spring Security

---
+++ api-gateway/src/main/java/com/maneesh/apigateway/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.maneesh.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
